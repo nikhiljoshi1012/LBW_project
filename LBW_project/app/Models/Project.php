d74743606f201d42457e3419135d60cb1934f5b4
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-06-11 00:30:48 +0530

    feat(routes and controllers): integrating Chirag's changes

---
+++ LBW_project/app/Models/Project.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'data',
+        'user_id'
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setCreatedAt($value)
+    {
+        $timezone = config('app.timezone', 'Asia/Kolkata');
+        $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $value, new \DateTimeZone($timezone));
+        $dateTime->setTimezone(new \DateTimeZone('Asia/Kolkata'));
+        $this->attributes['created_at'] = $dateTime->format('Y-m-d H:i:s');
+    }
+
+    public function setUpdatedAt($value)
+    {
+        $timezone = config('app.timezone', 'Asia/Kolkata');
+        $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $value, new \DateTimeZone($timezone));
+        $dateTime->setTimezone(new \DateTimeZone('Asia/Kolkata'));
+        $this->attributes['updated_at'] = $dateTime->format('Y-m-d H:i:s');
+    }
+}
