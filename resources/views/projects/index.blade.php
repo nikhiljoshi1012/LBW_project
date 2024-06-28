d6dfeab3b5256d16fe296f0d2b832506bb3fd703
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-06-28 19:39:01 +0530

    integrated datables

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,34 +1,57 @@
-@extends('layouts.app')
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Projects</h1>
-            <a href="{{ route('projects.create') }}" class="btn btn-primary">Create New Project</a>
-            <table class="table table-striped mt-3">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Date Created</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($projects as $project)
-                        <tr>
-                            <td>{{ $project->id }}</td>
-                            <td>{{ $project->name }}</td>
-                            <td>{{ $project->created_at }}</td>
-                            <td>
-                                <a href="{{ route('projects.show', $project->id) }}" class="btn btn-info">View</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<head>
+    <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    @extends('layouts.app')
+
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Projects</h1>
+                    <a href="{{ route('projects.create') }}" class="btn btn-primary">Create New Project</a>
+                    <table class="table table-striped mt-3" id="myTable">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Date Created</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <td>{{ $project->id }}</td>
+                                    <td>{{ $project->name }}</td>
+                                    <td>{{ $project->created_at }}</td>
+                                    <td>
+                                        <a href="{{ route('projects.show', $project->id) }}" class="btn btn-info">View</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-@endsection
+    @endsection
+
+    <!-- Corrected jQuery script tag -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- DataTables script should be after jQuery script -->
+    <script src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable(); // Corrected the selector
+        });
+    </script>
+</body>
+
+</html>
