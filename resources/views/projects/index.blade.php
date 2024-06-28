cf7d8b03db5d2a4ae847c7b3a9cb5cd1455eec94
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-06-28 19:43:22 +0530

    Fixed some bugs

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -15,27 +15,29 @@
                 <div class="col-md-12">
                     <h1>Projects</h1>
                     <a href="{{ route('projects.create') }}" class="btn btn-primary">Create New Project</a>
-                    <table class="table table-striped mt-3" id="myTable">
-                        <thead>
+                    <div class="container -mt5">
+                        <table class="table table-striped mt-3" id="myTable">
+                    </div>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Date Created</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
                             <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Date Created</th>
-                                <th>Actions</th>
+                                <td>{{ $project->id }}</td>
+                                <td>{{ $project->name }}</td>
+                                <td>{{ $project->created_at }}</td>
+                                <td>
+                                    <a href="{{ route('projects.show', $project->id) }}" class="btn btn-info">View</a>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($projects as $project)
-                                <tr>
-                                    <td>{{ $project->id }}</td>
-                                    <td>{{ $project->name }}</td>
-                                    <td>{{ $project->created_at }}</td>
-                                    <td>
-                                        <a href="{{ route('projects.show', $project->id) }}" class="btn btn-info">View</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
+                        @endforeach
+                    </tbody>
                     </table>
                 </div>
             </div>
