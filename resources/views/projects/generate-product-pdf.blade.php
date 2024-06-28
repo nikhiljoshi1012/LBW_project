12f2547651b7966527fc1cee40ced8308b42f00d
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-06-28 18:54:03 +0530

    update

--- resources/views/projects/generate-product-pdf.blade.php
+++ resources/views/projects/generate-product-pdf.blade.php
@@ -28,44 +28,55 @@
 <body>
     <h1 id="h1">{{ $title }}</h1>
     <br>
-    <p>Date : {{ $date }}</p>
+    <p>Date: {{ $date }}</p>
     <br>
 
     <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                @if (isset($output[0]) && (is_array($output[0]) || is_object($output[0])))
+                    @php
+                        $columns = is_array($output[0]) ? count($output[0]) : count((array) $output[0]);
+                    @endphp
+                    @for ($i = 1; $i <= $columns; $i++)
+                        <th>{{ $i }}</th>
+                    @endfor
+                @endif
+            </tr>
+        </thead>
         <tbody>
+            @php $rowNum = 1; @endphp <!-- Initialize row counter -->
             @if (is_array($output) || is_object($output))
-                @foreach ($output as $item)
-                    @if (is_array($item) || is_object($item))
-                        <tr>
+                @foreach ($output as $index => $item)
+                    <tr>
+                        <td>{{ chr(64 + $rowNum) }}</td> <!-- Display row letter (A, B, C, ...) -->
+                        @if (is_array($item) || is_object($item))
+                            @php $colNum = 1; @endphp <!-- Initialize column counter -->
                             @foreach ($item as $subItem)
                                 @php
-                                    $decodedSubItem = json_encode($subItem, JSON_UNESCAPED_UNICODE);
+                                    $decodedSubItem = json_decode(json_encode($subItem), true);
                                 @endphp
-                                @if (!is_numeric($decodedSubItem))
-                                    <td class="container" id="table-container" style="font-family: ome_bhatkhande_hindi">
-                                        <code style="font-family: ome_bhatkhande_hindi">{{ $decodedSubItem }}</code>
-                                    </td>
-                                @endif
-                            @endforeach
-                        </tr>
-                    @else
-                        @if (!is_numeric($item))
-                            <tr>
                                 <td class="container" id="table-container" style="font-family: ome_bhatkhande_hindi">
-                                    <code>{{ json_encode($item, JSON_UNESCAPED_UNICODE) }}</code>
+                                    <code style="font-family: ome_bhatkhande_hindi">{{ $decodedSubItem }}</code>
                                 </td>
-                            </tr>
+                                @php $colNum++; @endphp <!-- Increment column counter -->
+                            @endforeach
+                        @else
+                            <td class="container" id="table-container" style="font-family: ome_bhatkhande_hindi">
+                                <code>{{ json_decode(json_encode($item), true) }}</code>
+                            </td>
                         @endif
-                    @endif
+                    </tr>
+                    @php $rowNum++; @endphp <!-- Increment row counter -->
                 @endforeach
             @else
-                @if (!is_numeric($output))
-                    <tr>
-                        <td class="container" id="table-container" style="font-family: ome_bhatkhande_hindi">
-                            <code>{{ json_encode($output, JSON_UNESCAPED_UNICODE) }}</code>
-                        </td>
-                    </tr>
-                @endif
+                <tr>
+                    <td>{{ chr(64 + $rowNum) }}</td> <!-- Display row letter (A, B, C, ...) -->
+                    <td class="container" id="table-container" style="font-family: ome_bhatkhande_hindi">
+                        <code>{{ json_decode(json_encode($output), true) }}</code>
+                    </td>
+                </tr>
             @endif
         </tbody>
     </table>
