54767d5d56b8cfbf74f635d49e62bf599b1e5343
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-06-28 18:53:52 +0530

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,30 @@
+<head>
+    <link rel="stylesheet" href="{{ css('custom.css') }}">
+</head>
+
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Dashboard</h1>
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-        <a href="{{ route('projects.create') }}" class="btn btn-primary">Create New Project</a>
-        <h2>Your Projects</h2>
-        <ul>
-            @foreach ($projects as $project)
-                <li>
-                    <a href="{{ route('projects.show', $project->id) }}">{{ $project->name }}</a>
-                    <span>{{ $project->created_at }}</span>
-                </li>
-            @endforeach
-        </ul>
-    </div>
-@endsection
+
+    <body>
+
+        <div class="container">
+            <h1>Dashboard</h1>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <a href="{{ route('projects.create') }}" class="btn btn-primary create-project-btn">Create New Project</a>
+            <h2>Your Projects</h2>
+            <ul>
+                @foreach ($projects as $project)
+                    <li>
+                        <a href="{{ route('projects.show', $project->id) }}">{{ $project->name }}</a>
+                        <span>{{ $project->created_at }}</span>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endsection
+</body>
