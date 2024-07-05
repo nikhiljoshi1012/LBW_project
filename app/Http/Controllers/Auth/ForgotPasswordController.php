256d06c4c9a2f373a3885a5609bda39a68b227ee
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-07-05 18:59:56 +0530

    added email reset support and fixed bugs

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -30,26 +30,29 @@ class ForgotPasswordController extends Controller
        * @return response()
        */
       public function submitForgetPasswordForm(Request $request)
-      {
-          $request->validate([
-              'email' => 'required|email|exists:users',
-          ]);
-  
-          $token = Str::random(64);
-  
-          DB::table('password_resets')->insert([
-              'email' => $request->email, 
-              'token' => $token, 
-              'created_at' => Carbon::now()
-            ]);
-  
-          Mail::send('email.forgetPassword', ['token' => $token], function($message) use($request){
-              $message->to($request->email);
-              $message->subject('Reset Password');
-          });
-  
-          return back()->with('message', 'We have e-mailed your password reset link!');
-      }
+{
+    $request->validate([
+        'email' => 'required|email|exists:users',
+    ]);
+
+    $token = Str::random(64);
+
+    DB::table('password_resets')->updateOrInsert(
+        ['email' => $request->email],
+        [
+            'email' => $request->email, 
+            'token' => $token, 
+            'created_at' => Carbon::now()
+        ]
+    );
+
+    Mail::send('email.forgetPassword', ['token' => $token], function($message) use($request){
+        $message->to($request->email);
+        $message->subject('Reset Password');
+    });
+
+    return back()->with('message', 'We have e-mailed your password reset link!');
+}
       /**
        * Write code on Method
        *
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
@@ -43,7 +43,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('password_reset');
         Schema::dropIfExists('sessions');
     }
 };
