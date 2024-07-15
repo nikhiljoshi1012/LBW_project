804e0ddac1631de4e4fb307c679d6e5a0aaacd33
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-07-15 17:26:54 +0530

    Ui improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,58 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 
+    <style>
+        .uibtn {
+            --color: #ad0b0b;
+            font-family: inherit;
+            display: inline-block;
+            width: 8em;
+            height: 2.6em;
+            line-height: 2.5em;
+            margin: 20px;
+            position: relative;
+            overflow: hidden;
+            border: 2px solid var(--color);
+            transition: color .5s;
+            z-index: 1;
+            font-size: 17px;
+            border-radius: 6px;
+            font-weight: 500;
+            color: var(--color);
+            text-align: center;
+        }
+
+        .uibtn:before {
+            content: "";
+            position: absolute;
+            z-index: -1;
+            background: var(--color);
+            height: 150px;
+            width: 200px;
+            border-radius: 50%;
+        }
+
+        .uibtn:hover {
+            color: #fff;
+        }
+
+        .uibtn:before {
+            top: 100%;
+            left: 100%;
+            transition: all .7s;
+        }
+
+        .uibtn:hover:before {
+            top: -30px;
+            left: -30px;
+        }
+
+        .uibtn:active:before {
+            background: #a30c0c;
+            transition: background 0s;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -33,21 +85,24 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
                     </ul>
-
+                    <a href="{{ route('projects.recycleBin') }}" class="uibtn">Recycle Bin</a>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
@@ -65,7 +120,6 @@
                             @endif
                         @else
                             <!-- Inside your navigation or wherever you want the button -->
-                            <a href="{{ route('projects.recycleBin') }}" class="btn btn-primary">Recycle Bin</a>
 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
